fdf8ad9220323e6d696cd56052a162c38dd280d0
ACLA Design <acla.design.ltda@gmail.com>
2025-06-05 09:28:15 -0300

    Alt direta no trunk

--- arquivo-novo.py
+++ arquivo-novo.py
@@ -1,2 +1,2 @@
-texto = "Este é um arquivo novo da branch nova a ser incluído no trunk."
+texto = "Este é um arquivo novo da branch nova a ser incluído no trunk. Com alteração direta no trunk."
 print(texto)
